eda49b79d040f54a14eb2b651e3f3835000455b5
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-05-11 01:53:51 -0700

    added maps to first page and changed font, can't find where the pictures went

--- app.js
+++ app.js
@@ -100,3 +100,5 @@ tokyo.render();
 renderBottomTotals();
 
 
+
+
--- change.js
+++ change.js
@@ -1,12 +1,16 @@
 /// change the color of webpage
 var input = document.querySelector('input');
 var nav = document.querySelector('nav');
+var article = document.querySelector('article');
 
 input.addEventListener('change',
   function(){
     nav.style.setProperty('--nav-color', input.value);
   });
 
-
+input.addEventListener('change',
+  function(){
+    article.style.setProperty('--article-color', input.value);
+  });
 
 
